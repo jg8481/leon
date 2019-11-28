4002fd9c8e1498721ccc846a2c4094cd832ec0c5
Joshua Gorospe <jgorospe@fuzzproduction.com>
2019-11-27 22:20:20 -0500

    Updated the robotframeworktestassistant package

--- packages/robotframeworktestassistant/robotframeworkrunner.sh
+++ packages/robotframeworktestassistant/robotframeworkrunner.sh
@@ -34,7 +34,7 @@ if [ "$1" == "Test-Four" ]; then
   robot --include Test_Four --report NONE --log test-four-log.html --output test-four-output.xml -d $path/packages/robotframeworktestassistant/brain/Log-Files/Results $path/packages/robotframeworktestassistant/brain/Robot-Files/Leon-Robot-Framework-Customizable-Serial-Runner.robot > $path/packages/robotframeworktestassistant/brain/Log-Files/Test-Four-Log.txt
 fi
 
-if [ "$1" == "Set-Up-Runner-one" ]; then
+if [ "$1" == "Set-Up-Runner-One" ]; then
   path=$(pwd)
   rm -rf $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomSerialTestRunnerFile.csv
   touch $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomSerialTestRunnerFile.csv
@@ -74,6 +74,7 @@ if [ "$1" == "Custom-Runner-One" ]; then
   rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Results/custom-serial-test-run-*.xml
   rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Results/custom-serial-test-run-*.html
   #rm -rf $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomSerialTestRunnerFile.csv
+  cat $path/packages/robotframeworktestassistant/brain/Log-Files/Custom-Runner-One-Log.txt | slacktee.sh -i :nerd_face: --plain-text --config $path/packages/robotframeworktestassistant/brain/Tool-Strategies-Lone-Testers-Test-Leadership-Congress-2019/Workshop-Examples/Shared-Resources/.slacktee > /dev/null 2>&1
 fi
 
 if [ "$1" == "Display-Runner-One" ]; then
@@ -81,7 +82,83 @@ if [ "$1" == "Display-Runner-One" ]; then
   open $path/packages/robotframeworktestassistant/brain/Log-Files/Results/custom-serial-test-results-log.html
 fi
 
-if [ "$1" == "Slack-Notification" ]; then
+if [ "$1" == "Group-One" ]; then
+  path=$(pwd)
+  rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Group-One-Log.txt
+  touch $path/packages/robotframeworktestassistant/brain/Log-Files/Group-One-Log.txt
+  pabot --verbose --include Group_One --report NONE --log group-one-log.html --output group-one-output.xml -d $path/packages/robotframeworktestassistant/brain/Log-Files/Results $path/packages/robotframeworktestassistant/brain/Robot-Files/Leon-Robot-Framework-Customizable-Parallel-Runner*.robot > $path/packages/robotframeworktestassistant/brain/Log-Files/Group-One-Log.txt
+fi
+
+if [ "$1" == "Group-Two" ]; then
+  path=$(pwd)
+  rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Two-Log.txt
+  touch $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Two-Log.txt
+  pabot --verbose --include Group_Two --report NONE --log group-two-log.html --output group-two-output.xml -d $path/packages/robotframeworktestassistant/brain/Log-Files/Results $path/packages/robotframeworktestassistant/brain/Robot-Files/Leon-Robot-Framework-Customizable-Parallel-Runner*.robot > $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Two-Log.txt
+fi
+
+if [ "$1" == "Group-Three" ]; then
+  path=$(pwd)
+  rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Three-Log.txt
+  touch $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Three-Log.txt
+  pabot --verbose --include Group_Three --report NONE --log group-three-log.html --output group-three-output.xml -d $path/packages/robotframeworktestassistant/brain/Log-Files/Results $path/packages/robotframeworktestassistant/brain/Robot-Files/Leon-Robot-Framework-Customizable-Parallel-Runner*.robot > $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Three-Log.txt
+fi
+
+if [ "$1" == "Group-Four" ]; then
+  path=$(pwd)
+  rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Four-Log.txt
+  touch $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Four-Log.txt
+  pabot --verbose --include Group_Four --report NONE --log group-four-log.html --output group-four-output.xml -d $path/packages/robotframeworktestassistant/brain/Log-Files/Results $path/packages/robotframeworktestassistant/brain/Robot-Files/Leon-Robot-Framework-Customizable-Parallel-Runner*.robot > $path/packages/robotframeworktestassistant/brain/Log-Files/Group-Four-Log.txt
+fi
+
+if [ "$1" == "Set-Up-Runner-Two" ]; then
+  path=$(pwd)
+  rm -rf $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+  touch $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+  echo "Start" > $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+fi
+
+if [ "$1" == "Set-Group-One" ]; then
+  path=$(pwd)
+  echo "Group_One" >> $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+fi
+
+if [ "$1" == "Set-Group-Two" ]; then
+  path=$(pwd)
+  echo "Group_Two" >> $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+fi
+
+if [ "$1" == "Set-Group-Three" ]; then
+  path=$(pwd)
+  echo "Group_Three" >> $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+fi
+
+if [ "$1" == "Set-Group-Four" ]; then
+  path=$(pwd)
+  echo "Group_Four" >> $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+fi
+
+if [ "$1" == "Custom-Runner-Two" ]; then
+  path=$(pwd)
+  rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Custom-Runner-Two-Log.txt
+  touch $path/packages/robotframeworktestassistant/brain/Log-Files/Custom-Runner-Two-Log.txt
+  while IFS=, read -r PARALLELRUNNER
+  do
+    robot --include "${PARALLELRUNNER}" --report NONE --output custom-parallel-test-run-ouput.xml --log custom-parallel-test-run.html --timestampoutputs -N "${PARALLELRUNNER}" -d $path/packages/robotframeworktestassistant/brain/Log-Files/Results $path/packages/robotframeworktestassistant/brain/Robot-Files/Leon-Robot-Framework-Customizable-Parallel-Runner*.robot >> $path/packages/robotframeworktestassistant/brain/Log-Files/Custom-Runner-Two-Log.txt &&
+    sleep 0.5s
+  done < $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+  rebot --suitestatlevel 1 -N "Robot Framework test run with a manually constructed test sequence set up through leon-ai with a CustomParallelTestRunnerFile.csv file" --report NONE --log $path/packages/robotframeworktestassistant/brain/Log-Files/Results/custom-parallel-test-results-log.html --output $path/packages/robotframeworktestassistant/brain/Log-Files/Results/custom-parallel-test-run-*.xml >> $path/packages/robotframeworktestassistant/brain/Log-Files/Custom-Runner-Two-Log.txt
+  rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Results/custom-parallel-test-run-*.xml
+  rm -rf $path/packages/robotframeworktestassistant/brain/Log-Files/Results/custom-parallel-test-run-*.html
+  #rm -rf $path/packages/robotframeworktestassistant/brain/Robot-Files/CustomParallelTestRunnerFile.csv
+  cat $path/packages/robotframeworktestassistant/brain/Log-Files/Custom-Runner-Two-Log.txt | slacktee.sh -i :nerd_face: --plain-text --config $path/packages/robotframeworktestassistant/brain/Tool-Strategies-Lone-Testers-Test-Leadership-Congress-2019/Workshop-Examples/Shared-Resources/.slacktee > /dev/null 2>&1
+fi
+
+if [ "$1" == "Display-Runner-Two" ]; then
+  path=$(pwd)
+  open $path/packages/robotframeworktestassistant/brain/Log-Files/Results/custom-parallel-test-results-log.html
+fi
+
+if [ "$1" == "Slack-Notification-Send-All" ]; then
   path=$(pwd)
   cat $path/packages/robotframeworktestassistant/brain/Log-Files/*.txt | slacktee.sh -i :nerd_face: --plain-text --config $path/packages/robotframeworktestassistant/brain/Tool-Strategies-Lone-Testers-Test-Leadership-Congress-2019/Workshop-Examples/Shared-Resources/.slacktee > /dev/null 2>&1
 fi
--- packages/robotframeworktestassistant/robotframeworktestassistant.py
+++ packages/robotframeworktestassistant/robotframeworktestassistant.py
@@ -8,74 +8,129 @@ import sys
 import subprocess
 import re
 
-realpath = os.path.dirname(os.path.realpath(__file__))
+filepath = os.path.dirname(os.path.realpath(__file__))
 
 def Clean_Up_Results(string, entities):
     """Leon will clean up the results folder"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Clean-Up-Results', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Clean-Up-Results', shell=True)
     return utils.output('end', 'clean_up_results_ran', utils.translate('clean_up_results_ran'))
 
 def Test_One(string, entities):
     """Leon will run Robot Framework through the script runner"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Test-One', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Test-One', shell=True)
     return utils.output('end', 'single_test_ran', utils.translate('single_test_ran'))
 
 def Test_Two(string, entities):
     """Leon will run Robot Framework through the script runner"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Test-Two', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Test-Two', shell=True)
     return utils.output('end', 'single_test_ran', utils.translate('single_test_ran'))
 
 def Test_Three(string, entities):
     """Leon will run Robot Framework through the script runner"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Test-Three', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Test-Three', shell=True)
     return utils.output('end', 'single_test_ran', utils.translate('single_test_ran'))
 
 def Test_Four(string, entities):
     """Leon will run Robot Framework through the script runner"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Test-Four', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Test-Four', shell=True)
     return utils.output('end', 'single_test_ran', utils.translate('single_test_ran'))
 
 def Set_Up_Runner_One(string, entities):
     """Leon will set up a custom test run"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Set-Up-Runner-one', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Up-Runner-One', shell=True)
     return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
 
 def Set_Test_One(string, entities):
     """Leon will set up a custom test run"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Set-Test-One', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Test-One', shell=True)
     return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
 
 def Set_Test_Two(string, entities):
     """Leon will set up a custom test run"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Set-Test-Two', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Test-Two', shell=True)
     return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
 
 def Set_Test_Three(string, entities):
     """Leon will set up a custom test run"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Set-Test-Three', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Test-Three', shell=True)
     return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
 
 def Set_Test_Four(string, entities):
     """Leon will set up a custom test run"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Set-Test-Four', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Test-Four', shell=True)
     return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
 
 def Custom_Runner_One(string, entities):
     """Leon will start a custom Robot Framework test run"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Custom-Runner-One', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Custom-Runner-One', shell=True)
     return utils.output('end', 'multiple_tests_ran', utils.translate('multiple_tests_ran'))
 
 def Display_Runner_One(string, entities):
     """Leon will display the results of the Robot Framework test run"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Display-Runner-One', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Display-Runner-One', shell=True)
     return utils.output('end', 'display_results', utils.translate('display_results'))
 
-def Slack_Notification(string, entities):
+def Group_One(string, entities):
+    """Leon will run Robot Framework through the script runner"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Group-One', shell=True)
+    return utils.output('end', 'parallel_tests_ran', utils.translate('parallel_tests_ran'))
+
+def Group_Two(string, entities):
+    """Leon will run Robot Framework through the script runner"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Group-Two', shell=True)
+    return utils.output('end', 'parallel_tests_ran', utils.translate('parallel_tests_ran'))
+
+def Group_Three(string, entities):
+    """Leon will run Robot Framework through the script runner"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Group-Three', shell=True)
+    return utils.output('end', 'parallel_tests_ran', utils.translate('parallel_tests_ran'))
+
+def Group_Four(string, entities):
+    """Leon will run Robot Framework through the script runner"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Group-Four', shell=True)
+    return utils.output('end', 'parallel_tests_ran', utils.translate('parallel_tests_ran'))
+
+def Set_Up_Runner_Two(string, entities):
+    """Leon will set up a custom test run"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Up-Runner-Two', shell=True)
+    return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
+
+def Set_Group_One(string, entities):
+    """Leon will set up a custom test run"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Group-One', shell=True)
+    return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
+
+def Set_Group_Two(string, entities):
+    """Leon will set up a custom test run"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Group-Two', shell=True)
+    return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
+
+def Set_Group_Three(string, entities):
+    """Leon will set up a custom test run"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Group-Three', shell=True)
+    return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
+
+def Set_Group_Four(string, entities):
+    """Leon will set up a custom test run"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Set-Group-Four', shell=True)
+    return utils.output('end', 'finished_setting_up', utils.translate('finished_setting_up'))
+
+def Custom_Runner_Two(string, entities):
+    """Leon will start a custom Robot Framework test run"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Custom-Runner-Two', shell=True)
+    return utils.output('end', 'multiple_tests_ran', utils.translate('multiple_tests_ran'))
+
+def Display_Runner_Two(string, entities):
+    """Leon will display the results of the Robot Framework test run"""
+    subprocess.call(filepath + '/robotframeworkrunner.sh Display-Runner-Two', shell=True)
+    return utils.output('end', 'display_results', utils.translate('display_results'))
+
+def Slack_Notification_Send_All(string, entities):
     """Leon will send the console log results of the Robot Framework test runs to Slack"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Slack-Notification', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Slack-Notification-Send-All', shell=True)
     return utils.output('end', 'notify_the_team', utils.translate('notify_the_team'))
 
 def Robot_Framework_Desktop_Web_Test_Example(string, entities):
     """Leon will run Robot Framework through the script runner"""
-    subprocess.call(realpath + '/robotframeworkrunner.sh Robot-Framework-Desktop-Web-Test-Example', shell=True)
+    subprocess.call(filepath + '/robotframeworkrunner.sh Robot-Framework-Desktop-Web-Test-Example', shell=True)
     return utils.output('end', 'multiple_tests_ran', utils.translate('multiple_tests_ran'))
